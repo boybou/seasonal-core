345755368d17a7f51c0901a8512a31ad086c99bc
boy <boy.bouwense@gmail.com>
2022-02-01 20:36:00 +0000

    added constructor

--- src/entities/year.ts
+++ src/entities/year.ts
@@ -2,6 +2,33 @@ import { Quote } from "./quote";
 import { Trade } from "./trade";
 
 export class Year {
+  constructor(
+    year: number,
+    profitPercentage: number,
+    profit: number,
+    lowestChange: number,
+    highestChange: number,
+    highestChangeClose: number,
+    lowestChangeClose: number,
+    lowestPercentageChange: number,
+    highestPercentageChange: number,
+    lowestPercentageChangeClose: number,
+    highestPercentageChangeClose: number,
+    quotes: Quote[]
+  ) {
+    this.year = year;
+    this.profitPercentage = profitPercentage;
+    this.profit = profit;
+    this.lowestChange = lowestChange;
+    this.highestChange = highestChange;
+    this.highestChangeClose = highestChangeClose;
+    this.lowestChangeClose = lowestChangeClose;
+    this.lowestPercentageChange = lowestPercentageChange;
+    this.highestPercentageChange = highestPercentageChange;
+    this.lowestPercentageChangeClose = lowestPercentageChangeClose;
+    this.highestPercentageChangeClose = highestPercentageChangeClose;
+    this.quotes = quotes;
+  }
   id: number;
   year: number;
   profitPercentage: number;
